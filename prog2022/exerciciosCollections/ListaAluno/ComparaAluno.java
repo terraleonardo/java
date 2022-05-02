9c6cad3def2c4d9d8d64b295bc0b74630bd28a2e
Leonardo Terra <84455182+terraleonardo@users.noreply.github.com>
2022-05-02 16:29:36 -0300

    Create ComparaAluno.java

---
+++ prog2022/exerciciosCollections/ListaAluno/ComparaAluno.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+public class ComparaAluno implements Comparator<Aluno> {
+
+    public int compare(Aluno a, Aluno b) {
+         return a.getNome().compareTo(b.getNome());
+    }
+}
